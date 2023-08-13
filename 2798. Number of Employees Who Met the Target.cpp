ddad84f61e69b1263579de3c85caa3807d9babdf
Rajan Chaurasia <chaurasiarajan3013@gmail.com>
2023-08-13 19:18:47 +0530

    new

---
+++ 2798. Number of Employees Who Met the Target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int c=0;
+        for(auto i:hours)
+        {
+            if(i>=target)
+                c++;
+        }
+        return c;
+        
+    }
+};
